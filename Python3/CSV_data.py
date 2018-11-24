a0248894981875484425646ff6b9f2498b8adde9
Net <41178248+pawanrat11@users.noreply.github.com>
2018-11-25 01:02:17 +0700

    TEST

--- Python3/CSV_data.py
+++ Python3/CSV_data.py
@@ -3,3 +3,14 @@ Chicago-Crime-118 (PSIT Data Analysis Project)
 CSV Script
 """
 
+import csv
+file = open('Chicago_Crimes_2005_to_2007.csv')
+file2 = open('Chicago_Crimes_2001_to_2004.csv')
+data = csv.reader(file)
+data2 = csv.reader(file2)
+table = [row for row in data]
+table[:3]
+table2 = [row for row in data2]
+table[:3]
+print(table)
+print(table2)
